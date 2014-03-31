e7839a26e5e988c65c84fe41ef66ca6810b9d99e
gejove <gejove@163.com>
2014-03-31 11:41:08 +0800

    test git class

---
+++ testgit/src/com/gejove/test/TestClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.gejove.test;
+
+/**
+ * @author Gejove Lee
+ *
+ */
+public class TestClass {
+
+	/**
+	 * 
+	 */
+	public TestClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
